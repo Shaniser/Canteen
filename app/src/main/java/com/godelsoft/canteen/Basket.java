3c2b7ba215bab251eec990b405e62a0e23dee4e1
Shaniser <mr.shaniser@gmail.com>
2019-03-15 11:47:35 +0300

    Food card changes

--- app/src/main/java/com/godelsoft/canteen/Basket.java
+++ app/src/main/java/com/godelsoft/canteen/Basket.java
@@ -5,34 +5,68 @@ import java.util.HashMap;
 /**
  * Класс, описывающий корзину
  */
-public class Basket {
-    private HashMap<Integer, Integer> idToCount = new HashMap<>();
+public final class Basket {
+    private static HashMap<Integer, Integer> idToCount = new HashMap<>();
 
     /**
-     * Добавление еды в корзину
+     * Добавление еды в корзину (не больше 99)
      * @param food - вид еды
      * @param count - количество
      */
 
-    public void add(Food food, int count){
-        if(idToCount.containsKey(food.getId())) {
-            idToCount.put(food.getId(), idToCount.get(food.getId()) + count);
-        }else{
-            idToCount.put(food.getId(), count);
-        }
+    public static void add(Food food, int count){
+        add(food.getId(), count);
     }
 
     /**
-     * Добавление еды в корзину
+     * Добавление еды в корзину (не больше 99)
      * @param id - id вида еды
      * @param count - количество
      */
 
-    public void add(int id, int count){
+    public static void add(int id, int count){
         if(idToCount.containsKey(id)) {
-            idToCount.put(id, idToCount.get(id) + count);
+            idToCount.put(id, Math.min(idToCount.get(id) + count, 99));
         }else{
             idToCount.put(id, count);
         }
+    }
+
+    /**
+     * Удаляет count единиц еды id из корзины
+     * @param id
+     * @param count
+     * @return Произошло ли удаление
+     */
+
+    public static boolean remove(int id, int count){
+        if(idToCount.containsKey(id) && idToCount.get(id) > 0) {
+            idToCount.put(id, Math.max(idToCount.get(id) - count, 0));
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Удаляет count единиц еды food из корзины
+     * @param food
+     * @param count
+     * @return Произошло ли удаление
+     */
+
+    public static boolean remove(Food food, int count){
+        return remove(food.getId(), count);
+    }
+
+    /**
+     * Возвращает количество еды id в корзине
+     * @param id
+     * @return
+     */
+
+    public static int getCount(int id){
+        if(!idToCount.containsKey(id)) return 0;
+        return idToCount.get(id);
     }
 }
